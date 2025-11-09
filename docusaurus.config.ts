24aa19657549baa2c5b0fc3802c5e2e9d69ca978
hau2 <leconghau095@gmail.com>
2025-11-09 21:41:32 +0800

    Update lessions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
       title: 'Learn Mobile App Development',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
